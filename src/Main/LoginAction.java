bd6f1247a45e90ad0beb766ed98715282d8f4353
djangouser <sasakiwork16@gmail.com>
2025-11-07 11:23:52 +0900

    new

--- src/Main/LoginAction.java
+++ src/Main/LoginAction.java
@@ -1,5 +1,14 @@
 package Main;
 
-public class LoginAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+
+
+public class LoginAction {
+	public void execute(HttpServletRequest req, HttpServletResponse res)
+			throws Exception {
+
+		req.getRequestDispatcher("login.jsp").forward(req, res);
+	}
 }
