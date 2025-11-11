2cefeb22b70f12734eb483e039fe0db9b39fbde6
djangouser <sasakiwork16@gmail.com>
2025-11-11 12:24:33 +0900

    c

--- src/dao/ClassNumDao.java
+++ src/dao/ClassNumDao.java
@@ -1,5 +1,44 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import bean.ClassNum;
+
 public class ClassNumDao extends Dao{
 
+
+	//クラスを選択後、クラス情報の取得
+	public ArrayList<ClassNum> get(int classnum) throws Exception{
+		ArrayList<ClassNum> num = new ArrayList();
+		Connection connection = getConnection();
+		PreparedStatement statement = null;
+
+		try{
+			statement = connection.prepareStatement("");
+
+		}catch(Exception e){
+			throw e;
+		}finally{
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+			//コネクションを閉じる
+			if (connection != null){
+				try {
+					connection.close();
+				} catch (SQLException sqle){
+					throw sqle;
+				}
+			}
+		}
+
+		return num;
+	}
 }
--- src/dao/TotalAbsencesDao.java
+++ src/dao/TotalAbsencesDao.java
@@ -1,8 +1,42 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import bean.TotalAbsences;
+
 public class TotalAbsencesDao extends Dao{
 
-	
+	public TotalAbsences get(int id) throws Exception{
+		TotalAbsences total = new TotalAbsences();
+		Connection connection = getConnection();
+		PreparedStatement statement = null;
+
+		try{
+			statement = connection.prepareStatement("");
+
+		}catch(Exception e){
+			throw e;
+		}finally{
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+			//コネクションを閉じる
+			if (connection != null){
+				try {
+					connection.close();
+				} catch (SQLException sqle){
+					throw sqle;
+				}
+			}
+		}
+		return total;
+	}
 
 
 
