27927e6d5209813c7bfbbc0a7c63c90026f31fc0
djangouser <sasakiwork16@gmail.com>
2025-11-13 12:18:39 +0900

    test

--- src/tool/QRcodeServlet.java
+++ src/tool/QRcodeServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.zxing.*;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
@@ -30,8 +32,8 @@ public class QRcodeServlet extends HttpServlet {
         int size = 200;
         try {
             BitMatrix matrix = new MultiFormatWriter().encode(qrUrl, BarcodeFormat.QR_CODE, size, size);
-            response.setContentType("image/png");
-            MatrixToImageWriter.writeToStream(matrix, "png", response.getOutputStream());
+            res.setContentType("image/png");
+            MatrixToImageWriter.writeToStream(matrix, "png", res.getOutputStream());
         } catch (WriterException e) {
             e.printStackTrace();
         }
