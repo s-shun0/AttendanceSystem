4af4ab91106f84e35bfbf2b25d6cb574aea8b61e
a_shijo <a_shijo@10.202.27.149>
2025-11-25 10:08:05 +0900

    now

--- src/tool/EncodingFilter.java
+++ src/tool/EncodingFilter.java
@@ -8,27 +8,28 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 
-@WebFilter(urlPatterns = { "/*" })
 public class EncodingFilter implements Filter {
-	/**
-	 * doFilterメソッド フィルター処理を記述
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		// 文字コードをセット
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charset=UTF-8");
 
-		// System.out.println("フィルタの前処理");
-		chain.doFilter(request, response);
-		// System.out.println("フィルタの後処理");
-	}
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 
-	public void init(FilterConfig filterConfig) {
-	}
+            throws IOException, ServletException {
 
-	public void destroy() {
-	}
+        request.setCharacterEncoding("UTF-8");
+
+        response.setContentType("text/html; charset=UTF-8");
+
+        chain.doFilter(request, response);
+
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
+
 }
+
