54ce9476491a9bc38a1bdc5880ea27ad673ca448
a_shijo <a_shijo@10.202.27.149>
2025-11-26 12:25:09 +0900

    クラスセレクト、クラスの生徒情報一覧機能完成

--- src/Teacher/ClassSelectExecuteAction.java
+++ src/Teacher/ClassSelectExecuteAction.java
@@ -14,22 +14,18 @@ public class ClassSelectExecuteAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 	    try {
-	    	req.getSession().removeAttribute("classnum"); // ← ここで削除
 	        // リクエストパラメータ―の取得
 	        int classnum = Integer.parseInt(req.getParameter("classnum"));
-
-	        req.getSession().setAttribute("classnum", classnum);
-
 
 	        // DBからクラスに基づいたデータの取得
 	        UserDao userDao = new UserDao();
 	        List<User> students = userDao.class_(classnum);
 
-	        // リクエストスコープに格納して JSP にフォワード
-	        req.setAttribute("students", students);
+	        req.getSession().setAttribute("students", students);
+
 	        req.getRequestDispatcher("/attendsystem/Teacher/AttendanceTracker.action").forward(req, res);
 
-	        
+
 	    } catch (Exception e) {
 	        // 何らかのエラーが発生したら error.jsp に飛ばす
 	        req.getRequestDispatcher("error.jsp").forward(req, res);
